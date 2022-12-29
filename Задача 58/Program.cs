bb8a61268b66f67f0552eb3c791648bb9e19f936
Lupino <vladislav_rozhno@mail.ru>
2022-12-29 20:20:29 +0500

    Задача 58

--- Задача 58/Program.cs
+++ Задача 58/Program.cs
@@ -1,55 +1,71 @@
-﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
+Console.Clear();
+Console.WriteLine($"Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.\n\nСразу зададим матрицу, которую можно перемножить, т.е. количество столбцов первой равно количеству строк второй");
+Console.WriteLine($"\nВведите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
 
-void Sort(int[,] array)
+int[,] firstMartrix = new int[m, n];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+WriteArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+WriteArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+WriteArray(resultMatrix);
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
   {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
     {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
       {
-        if (array[i, k] < array[i, k + 1])
-        {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
-        }
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
       }
+      resultMatrix[i,j] = sum;
     }
   }
 }
 
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
 
-
-void GetArray(int[,] array)
+void CreateArray(int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      array[i, j] = new Random().Next(0, 99);
+      array[i, j] = new Random().Next(range);
     }
   }
 }
 
-void Print(int[,] array)
+void WriteArray (int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      Console.Write(array[i, j] + " ");
+      Console.Write(array[i,j] + " ");
     }
     Console.WriteLine();
   }
 }
-
-int m = int.Parse(Console.ReadLine());
-int n = int.Parse(Console.ReadLine());
-int[,] array = new int[m, n];
-GetArray(array);
-Print(array);
-Console.WriteLine($"\nОтсортированный массив: ");
-Sort(array);
-Print(array);
